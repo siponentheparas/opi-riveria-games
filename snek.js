882ed606d9146c13609fccbf91c407d32f9c8d91
siponen <osilvenn@gmail.com>
2024-03-20 08:57:24 +0200

    cool setupping thiungs

--- snek.js
+++ snek.js
@@ -1,14 +1,38 @@
-javascript: (() => {
+javascript: (async () => {
+
+    function delay(duration) {
+        return new Promise(resolve => setTimeout(resolve, duration));
+    }
+
     let brc_selector = document.querySelector("div[class=barRowCells]");
     let brc_elements = brc_selector.getElementsByTagName('*');
+
     for (i = 0; i < brc_elements.length; i++) {
         brc_elements[i].className = "progressBarCell completed";
     }
 
-    for (i = 0; i < 156 - brc_elements.length; i++) {
-        setTimeout(function() {
+    async function add_cells() {
+        for (i = 0; i < 156 - brc_elements.length; i++) {
+            await delay(10);
             let cloned_div = brc_elements[0].cloneNode(true);
             brc_selector.appendChild(cloned_div);
-        }, i * 10);
+        }
     }
+
+    /* This is horrible and I don't like it. But there's not any other way to do this */
+    async function add_add_cells() {
+        while (brc_elements.length != 156) {
+            await add_cells();
+        }
+    }
+
+    async function setup_cells() {
+        for (i = 0; i < brc_elements.length; i++) {
+            await delay(10);
+            brc_elements[i].className = "progressBarCell futureNotCompleted";
+        }
+    }
+
+    await add_add_cells();
+    await setup_cells();
 })()
