18d272ac23381b73cf0d148d833879f81328c03a
siponen <osilvenn@gmail.com>
2024-03-20 08:10:38 +0200

    cool lookin starting thiungy

--- snek.js
+++ snek.js
@@ -1,12 +1,14 @@
-javascript:(() => {
+javascript: (() => {
     let brc_selector = document.querySelector("div[class=barRowCells]");
     let brc_elements = brc_selector.getElementsByTagName('*');
     for (i = 0; i < brc_elements.length; i++) {
         brc_elements[i].className = "progressBarCell completed";
     }
 
-    for (i = brc_elements.length; i < 169; i++) {
-        let cloned_div = brc_elements[0].cloneNode(true);
-        brc_selector.appendChild(cloned_div);
+    for (i = 0; i < 156 - brc_elements.length; i++) {
+        setTimeout(function() {
+            let cloned_div = brc_elements[0].cloneNode(true);
+            brc_selector.appendChild(cloned_div);
+        }, i * 10);
     }
 })()
